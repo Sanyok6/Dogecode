5099b1924c20764213f38b6862578d338a52057f
Sanyok6 <tishchenkoa2008@gmail.com>
2022-07-17 08:56:28 -0400

    Quick fix - all authenticated users can now post.

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -12,7 +12,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return bool(
             request.method in permissions.SAFE_METHODS or
             request.user and
-            request.user.is_staff
+            request.user.is_authenticated
         )
 
 
